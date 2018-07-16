0db4780980d72b6390e1515908bd39e747ab7c6f
bjsmith3185 <bjsmith3185@hotmail.com>
2018-07-16 14:57:22 -0400

    tring to get a reset working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,59 +9,97 @@ $( document ).ready(function() {
     $(".container-win").hide();
     $(".container-loss").hide();
 
+    var sprites = {
+        "walker": {
+            name: "walker",
+            image: "assets/images/walker.jpg",
+            health: 12,
+            attack: 3,
+            counter: 5,
+        },
+    
+        "trivette": {
+            name: "trivette",
+            image: "assets/images/trivette.jpg",
+            health: 18,
+            attack: 10,
+            counter: 6,
+        },
+    
+        "cb": {
+            name: "cb",
+            image: "assets/images/cb.jpg",
+            health: 10,
+            attack: 8,
+            counter: 5,
+        },
+    
+        "gage": {
+            name: "gage",
+            image: "assets/images/gage.jpg",
+            health: 12,
+            attack: 7,
+            counter: 4,
+        },
+    };
+
+    console.log("walker's health: " + sprites["walker"].health);
+
     var userSelected = false;
     var userHP;
     var userAttack;
-    var userCounter;
+    // var userCounter;
     var userImg;
     var userName;
     var otherCharacters;
     
    
     var enemyHP;
-    var enemyAttack;
+    // var enemyAttack;
     var enemyCounter;
     var enemyName;
 
     var count = 3;
 
-var sprites = {
-    "walker": {
-        name: "walker",
-        image: "assets/images/walker.jpg",
-        health: 12,
-        attack: 3,
-        counter: 5,
-    },
+// reset function 
+    function reset() {
+        $(".container-welcome").show();
+        $(".container-select-user").hide();
+        $(".container-select-enemy").hide();
+        $(".container-game").hide();
+        $(".container-win").hide();
+        $(".container-loss").hide();
+        
+        $("#start-select").empty();
+        $(".userSprite-game").empty();
+        
+        userHP;
+        userAttack;
+        userImg;
+        userName;
+        otherCharacters;
 
-    "trivette": {
-        name: "trivette",
-        image: "assets/images/trivette.jpg",
-        health: 18,
-        attack: 10,
-        counter: 6,
-    },
+        enemyHP;
+        enemyCounter;
+        enemyName;
+        count = 3;
 
-    "cb": {
-        name: "cn",
-        image: "assets/images/cb.jpg",
-        health: 10,
-        attack: 8,
-        counter: 5,
-    },
 
-    "gage": {
-        name: "gage",
-        image: "assets/images/gage.jpg",
-        health: 12,
-        attack: 7,
-        counter: 4,
-    },
-};
+    };
 
-// var count1 = sprites.keys.length;
-// console.log(count1 + " this is object length");
 
+// display sprites to div area 
+    function displaySprite(pass) {
+        var boxName = $("<div>").attr("data-name", pass.name).attr("data-health",pass.health).attr("data-attack", pass.attack).attr("data-counter", pass.counter).attr("data-img", pass.image).addClass("sprite", "text-center");
+        var boxTitle = $("<p>").addClass("title", "text-center").text(pass.name);
+        var boxPic = $("<img>").attr("src", pass.image).addClass("sprite-img", "text-center");
+        var boxInfo = $("<p>").addClass("info", "text-center").text("Health: " + pass.health);
+        boxName.append(boxTitle).append(boxPic).append(boxInfo);
+        $("#start-select").append(boxName);
+    };
+
+
+// welcome button area 
 
     $("#welcome-btn").on("click", function() {
         $(".container-welcome").hide();
@@ -70,15 +108,8 @@ var sprites = {
         $(".container-game").hide();
         $(".container-win").hide();
         $(".container-loss").hide();
-
-        function displaySprite(pass) {
-            var boxName = $("<div>").attr("data-name", pass.name).attr("data-health",pass.health).attr("data-attack", pass.attack).attr("data-counter", pass.counter).attr("data-img", pass.image).addClass("sprite", "text-center");
-            var boxTitle = $("<p>").addClass("title", "text-center").text(pass.name);
-            var boxPic = $("<img>").attr("src", pass.image).addClass("sprite-img", "text-center");
-            var boxInfo = $("<p>").addClass("info", "text-center").text("Health: " + pass.health);
-            boxName.append(boxTitle).append(boxPic).append(boxInfo);
-            $("#start-select").append(boxName);
-        };
+        
+        
       
       
         for (var key in sprites) {
@@ -93,7 +124,7 @@ var sprites = {
         
         userHP = parseInt($(this).attr("data-health"));
         userAttack = parseInt($(this).attr("data-attack"));
-        userCounter = parseInt($(this).attr("data-counter"));
+        // userCounter = parseInt($(this).attr("data-counter"));
         userName = $(this).attr("data-name");
         userImg = $(this).attr("data-img");
 
@@ -115,29 +146,29 @@ var sprites = {
 
 // pick first enemy 
 
-$(".container-select-enemy").on("click", ".sprite",  function() {
+    $(".container-select-enemy").on("click", ".sprite",  function() {
+            
+        enemyHP = parseInt($(this).attr("data-health"));
+        // enemyAttack = parseInt($(this).attr("data-attack"));
+        enemyCounter = parseInt($(this).attr("data-counter"));
+        enemyName = $(this).attr("data-name");
+        enemyImg = $(this).attr("data-img");
+        console.log("this is enemyImg: " + enemyImg);
         
-    enemyHP = parseInt($(this).attr("data-health"));
-    enemyAttack = parseInt($(this).attr("data-attack"));
-    enemyCounter = parseInt($(this).attr("data-counter"));
-    enemyName = $(this).attr("data-name");
-    enemyImg = $(this).attr("data-img");
-    console.log("this is enemyImg: " + enemyImg);
     
-   
 
-    $(this).detach().appendTo(".enemySprite-game");
-    
-    
-    $(".container-welcome").hide();
-    $(".container-select-user").hide();
-    $(".container-select-enemy").hide();
-    $(".container-game").show();
-    $(".container-win").hide();
-    $(".container-loss").hide();
-});
+        $(this).detach().appendTo(".enemySprite-game");
+        
+        
+        $(".container-welcome").hide();
+        $(".container-select-user").hide();
+        $(".container-select-enemy").hide();
+        $(".container-game").show();
+        $(".container-win").hide();
+        $(".container-loss").hide();
+    });
 
-    // attack area 
+// attack area 
 
     $("#attack").on("click", function() {
         console.log("this is enemyName: " + enemyName + "user name: " + userName);
@@ -176,54 +207,26 @@ $(".container-select-enemy").on("click", ".sprite",  function() {
                 $(".container-select-enemy").show();
                 $(".container-game").hide();
 
-            }
+            };
             
-        }
+        };
        
         function setImage(a,b,x,y) {
             a.attr("src", b);
             x.text(y);
 
         };
-
-
-
-
-
-
-
-        // function setImage(y,x) { //l
-        //     // console.log("this is enemyName: " + enemyName + "user name: " + userName);
-            
-          
-        //     if (x === "sprite-1") {
-        //         $(y).attr("src", "assets/images/walker.jpg");
-        //     } else if (x === "sprite-2") {
-        //         $(y).attr("src", "assets/images/cb.jpg");
-        //     } else if (x === "sprite-3") {
-        //         $(y).attr("src", "assets/images/trivette.jpg");
-        //     } else if (x === "sprite-4") {
-        //         $(y).attr("src", "assets/images/gage.jpg");
-        //     }
-        // };
+       
 
     });
 
-//     function reset() {
-//         $(".container-welcome").show();
-//         $(".container-select-user").hide();
-//         $(".container-select-enemy").hide();
-//         $(".container-game").hide();
-//         $(".container-win").hide();
-//         $(".container-loss").hide();
-//     };
+    
+    $("#reset-win").on("click", function() {
+        reset();
+    });
 
-//     $("#reset-win").on("click", function() {
-//         reset();
-//     });
-
-//     $("#reset-loss").on("click", function() {
-//         reset();
-//     });
+    $("#reset-loss").on("click", function() {
+        reset();
+    });
 
 });
